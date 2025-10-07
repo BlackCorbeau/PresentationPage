e18e58960c3452269b485f8fdb0f00d05425230e
Kirill <kremizov3@gmail.com>
2025-10-07 21:19:55 +0300

    feat(app.py): Connected routes
    Added connection to app from routs package

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from routes import index, selfPage
 def createAPP():
     app = Flask(__name__)
 
+    app.register_blueprint(index)
+    app.register_blueprint(selfPage)
     # Тут будет инициадизация бд и подключение модулей
 
     return app 
